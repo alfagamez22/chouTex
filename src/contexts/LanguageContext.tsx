2dd8f693d6440cb017cb75a69c84c6b35f341265
alfagamez22 <harveybuan1234@gmail.com>
2025-10-26 13:41:50 +0100

    WIP: Live update does work for language switching

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -88,7 +88,6 @@ export const LanguageProvider: React.FC<LanguageProviderProps> = ({ children }) 
                 label: `${lang.nativeName} (${lang.name})`,
                 value: lang.code
             })),
-            liveUpdate: false,
             onChange: (value) => {
                 changeLanguage(value as string);
             }
