9c19c42a8895197ed471ea73261464cd9d599ba5
alfagamez22 <harveybuan1234@gmail.com>
2026-02-04 19:04:17 +0100

    WIP: Updated drawio icon

--- extras/viewers/drawio/Icon.tsx
+++ extras/viewers/drawio/Icon.tsx
@@ -11,15 +11,12 @@ export const DrawioIcon: React.FC = () => (
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
-        strokeLinejoin="round">
-        <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
-        <circle cx="8" cy="8" r="2" />
-        <circle cx="16" cy="8" r="2" />
-        <circle cx="8" cy="16" r="2" />
-        <circle cx="16" cy="16" r="2" />
-        <line x1="10" y1="8" x2="14" y2="8" />
-        <line x1="8" y1="10" x2="8" y2="14" />
-        <line x1="16" y1="10" x2="16" y2="14" />
-        <line x1="10" y1="16" x2="14" y2="16" />
+        strokeLinejoin="round"
+    >
+        <rect x="9" y="3" width="6" height="6" rx="1.5" />
+        <rect x="3" y="15" width="6" height="6" rx="1.5" />
+        <rect x="15" y="15" width="6" height="6" rx="1.5" />
+        <line x1="12" y1="9" x2="6" y2="15" />
+        <line x1="12" y1="9" x2="18" y2="15" />
     </svg>
 );
