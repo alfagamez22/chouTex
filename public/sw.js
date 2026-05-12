304ed96b7a1184adc3592ab89f38b368908e0891
alfagamez22 <harveybuan1234@gmail.com>
2026-05-12 22:58:55 +0200

    Bumped version

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // These constants are automatically generated. Do not edit directly.
-// Generated on: 2026-05-12T19:38:48.232Z
-const CACHE_NAME = `texlyre-v0.7.51`;
+// Generated on: 2026-05-12T20:58:07.630Z
+const CACHE_NAME = `texlyre-v0.7.52`;
 const BASE_PATH = '/texlyre/';
 const FONTS_CACHE_NAME = 'fonts-cache-v1';
 const AIRGAP_ALLOWED_DOMAINS = [
